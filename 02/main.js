6bcad58c4b85dcd90feb67833b9c326c8712c510
Danny H <6776962+thedannydarko@users.noreply.github.com>
2020-12-06 09:48:43 -0500

    Completed day 2 part 2

--- 02/main.js
+++ 02/main.js
@@ -13,7 +13,7 @@ async function getInput() {
     })
 } 
 
-const checkValidity = function(pwLine) {
+const checkValidityP1 = function(pwLine) {
     let minMax, min, max, requiredLetter, password;
     [minMax, requiredLetter, password] = pwLine.split(" ");
     [min, max] = minMax.split("-");
@@ -32,12 +32,25 @@ const checkValidity = function(pwLine) {
     return false;
 }
 
-async function solve() {
-    let input = (await getInput()).split("\n");
+const checkValidityP2 = function(pwLine) {
+    let positions, pos1, pos2, requiredLetter, password;
+    [positions, requiredLetter, password] = pwLine.split(" ");
+    [pos1, pos2] = positions.split("-");
+    requiredLetter = requiredLetter.split(":")[0];
 
+    let c1 = password.charAt(pos1 - 1);
+    let c2 = password.charAt(pos2 - 1);
+
+    if ((c1 === requiredLetter || c2 === requiredLetter) && (c1 !== c2)) {
+        return true;
+    }
+    return false;
+}
+
+async function solvePart1(input) {
     let totalValidPasswords = 0;
     for (let i = 0; i < input.length; i++) {
-        if (checkValidity(input[i])) {
+        if (checkValidityP1(input[i])) {
             totalValidPasswords++;
         }
     }
@@ -45,6 +58,27 @@ async function solve() {
     return totalValidPasswords;
 }
 
-solve()
-.then(totalValidPasswords => 
-    console.log("Total valid passwords:", totalValidPasswords));
+async function solvePart2(input) {
+    let totalValidPasswords = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (checkValidityP2(input[i])) {
+            totalValidPasswords++;
+        }
+    }
+
+    return totalValidPasswords;
+}
+
+async function solve() {
+    let input = (await getInput()).split("\n");
+
+    solvePart1(input)
+    .then(totalValidPasswords => 
+        console.log("Total valid passwords part 1:", totalValidPasswords));
+
+    solvePart2(input)
+    .then(totalValidPasswords => 
+        console.log("Total valid passwords part 2:", totalValidPasswords));
+}
+
+solve();
