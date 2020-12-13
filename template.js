69c8620186bd6b41701855f66fa4cc7d525b77b3
Danny H <6776962+thedannydarko@users.noreply.github.com>
2020-12-12 22:12:11 -0500

    Made template to use for each day

---
+++ template.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const {getInput} = require('../helper')
+
+/**
+ * @param {Array<String>} input
+ * @return {Promise<Number>}
+ */
+async function solvePartOne(input) {
+    return 0;
+}
+
+/**
+ * @param {Array<String>} input
+ * @return {Promise<Number>}
+ */
+async function solvePartTwo(input) { // > 24643
+    return 0;
+}
+
+async function solve() {
+    let inFile = path.resolve(__dirname, "input")
+    let input = (await getInput(inFile)).split("\n");
+    
+    solvePartOne(Array.from(input))
+        .then(num => console.log("Part 1 solution:", num));
+
+    solvePartTwo(Array.from(input))
+        .then(num => console.log("Part 2 solution:", num));
+}
+
+solve();
